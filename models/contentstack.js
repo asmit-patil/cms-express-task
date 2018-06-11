a505b291e66714bff6dd3c3b3a55182df12ac4ff
asmit patil <patilasmit0@gmail.com>
2018-06-11 16:07:58 +0530

    fetching contentdata using contentstack sdk

--- models/contentstack.js
+++ models/contentstack.js
@@ -1,44 +1,3 @@
-// var request = require('request')
-// var promise = 
-// {
-//     promiseFunction : function (url){
-//         return new Promise(function(resolve, reject) {
-//                 request.get( options = {  
-//                         url: url  
-//         }, function(err, resp, body) {
-//         console.log(resp.statusCode,"______________status code")
-//         if (err) {
-//             return reject(err)
-//         }else if (resp.statusCode >= 200 && resp.statusCode < 300) {
-//              console.log(typeof JSON.parse(body),"_____________body type")
-//              //body="string"
-//              return resolve(JSON.parse(body))
-//         } else {
-//             if((typeof (JSON.parse(body))) != "object"){
-//                 res.render('error')
-//             }
-//         }
-//         })     
-//     })
-//     },
-//     promiseAll : function(urlArray, viewName, res){
-//         Promise.all(urlArray)
-//             .then(function(values) 
-//             {   
-//                 var data
-//                 console.log( values, "______________result")
-//                 res.render(viewName, {
-//                         data : values
-//                 })
-//             })
-//             .catch(function(err){
-//                 console.log(err)
-//             })   
-//     }
-
-// }
-// module.exports = promise
-
 var Contentstack = require('contentstack')
 var config = require('../configuration/constants')
 const Stack = Contentstack.Stack(config.apikey, config.accesstoken, config.enviournment);
@@ -53,9 +12,6 @@ var promise =
         .toJSON()
         .find()
         .spread(function success(result) {
-            // res.render('pages/home/index.html', {
-            //     entry: result[0],
-            // })
           console.log(result,"-------------result")
             return resolve(result[0])
         }, function error(error) {
