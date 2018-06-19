9fe331b6f014e7d713801d70c8de14bd4bb6e61a
asmitpatil <asmitnpatil11@gmail.com>
2018-06-20 00:45:07 +0530

    making changes is partails

--- middlewares/partials.js
+++ middlewares/partials.js
@@ -4,9 +4,9 @@ var config = require('config')
 const Stack = Contentstack.Stack(config.get('apikey'), config.get('accesstoken'), config.get('enviournment'))
 
 module.exports = function (req, res, next) {
-	
+		var contentTypes = ["header", "footer", "home"]
 		async.map(
-			["header", "footer", "home"],
+			contentTypes,
 			function (contentType, callback) {
 						Stack.ContentType(contentType).Query()
 						.toJSON()
@@ -22,17 +22,10 @@ module.exports = function (req, res, next) {
 						
 			},
 			function (err, success) {
-				if (err) {
-					console.error(err)
-				  }
-			console.log("in cb function")
-			console.log(success[0], "success data")
-			console.log(success.length)
-			res.locals.partials = {}
-			res.locals.partials.header = success[0]
-			res.locals.partials.footer = success[1]
-		
-			next()
+				if (err) console.error(err)
+				res.locals.partials = {}
+				contentTypes.forEach((key,result)=> res.locals.partials[key] = success[result])
+				next()
 			}
 		)	
 }
